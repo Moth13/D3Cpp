11467b3e62e8ac60acc65aba60fc50dbb28221a7
moth13 <moth13@70b62f85-e027-4081-bb3b-d635e6dceebc>
2012-12-19 09:35:30 +0000

    git-svn-id: svn://svn.code.sf.net/p/d3cplus/code-0/trunk@8 70b62f85-e027-4081-bb3b-d635e6dceebc

--- d3cplus/include/d3cplus_data_item.h
+++ d3cplus/include/d3cplus_data_item.h
@@ -7,15 +7,51 @@ namespace d3cplus
 {
     namespace Data
     {
+        class D3_Item;
+
+        typedef QPair< QString, QString > D3_Item_SubInfo;
+
+        struct D3_Item_Salvage
+        {
+            float       m_fChance;
+            uint        m_uiQuantity;
+            D3_Item*    m_pSalvage;
+
+            explicit D3_Item_Salvage();
+            ~D3_Item_Salvage();
+        };
+        struct D3_Item_Gem
+        {
+            D3_Item*    m_pGem;
+
+            explicit D3_Item_Gem();
+            ~D3_Item_Gem();
+        };
+
         /*! \class*/
         class D3_Item: public D3_Data
         {
                 Q_OBJECT
-                Q_PROPERTY(QString id READ getID WRITE setID)
-                Q_PROPERTY(QString name READ getName WRITE setName)
-                Q_PROPERTY(QString icon READ getIcon WRITE setIcon)
-                Q_PROPERTY(QString displayColor READ getDisplayColor WRITE setDisplayColor)
-                Q_PROPERTY(QString tooltipParams READ getTooltipParams WRITE setTooltipParams)
+                Q_PROPERTY(QString id                   READ getID                      WRITE setID)
+                Q_PROPERTY(QString name                 READ getName                    WRITE setName)
+                Q_PROPERTY(QString icon                 READ getIcon                    WRITE setIcon)
+                Q_PROPERTY(QString displayColor         READ getDisplayColor            WRITE setDisplayColor)
+                Q_PROPERTY(QString tooltipParams        READ getTooltipParams           WRITE setTooltipParams)
+                Q_PROPERTY(QString requiredLevel        READ getRequiredLevel           WRITE setRequiredLevel)
+                Q_PROPERTY(QString itemLevel            READ getItemLevel               WRITE setItemLevel)
+                Q_PROPERTY(QString bonusAffixes         READ getBonusAffixes            WRITE setBonusAffixes)
+                Q_PROPERTY(QString typeName             READ getTypeName                WRITE setTypeName)
+                Q_PROPERTY(QString type                 READ getType                    WRITE setType)
+                Q_PROPERTY(QVariant dps                 READ getDpsAsQVariant           WRITE setDps)
+                Q_PROPERTY(QVariant attacksPerSecond    READ getAttackPerSecAsQVariant  WRITE setAttackPerSec)
+                Q_PROPERTY(QVariant minDamage           READ getMinDamageAsQVariant     WRITE setMinDamage)
+                Q_PROPERTY(QVariant maxDamage           READ getMaxDamageAsQVariant     WRITE setMaxDamage)
+                Q_PROPERTY(QStringList attributes       READ getAttributs               WRITE setAttributs)
+                Q_PROPERTY(QVariant attributesRaw       READ getAttributsRawAsQVariant  WRITE setAttributsRaw)
+                Q_PROPERTY(QVariant socketEffets        READ getSocketEffectsAsQVariant WRITE setSocketEffects)
+                Q_PROPERTY(QVariant salvage             READ getSalvageAsQVariant       WRITE setSalvage)
+                Q_PROPERTY(QVariant gems                READ getGemsAsQVariant          WRITE setGems)
+
 
             public:
                 explicit D3_Item( QObject* parent = NULL );
@@ -23,18 +59,55 @@ namespace d3cplus
                 virtual ~D3_Item();
 
                 // setFunction
-                void setID( const QString& _rstrID );
-                void setName( const QString& _rstrName );
-                void setIcon( const QString& _rstrIcon);
-                void setDisplayColor( const QString& _rstrDisplayColor );
-                void setTooltipParams( const QString& _rstrTooltipParams );
+                void setID              ( const QString& _rstrID );
+                void setName            ( const QString& _rstrName );
+                void setIcon            ( const QString& _rstrIcon);
+                void setDisplayColor    ( const QString& _rstrDisplayColor );
+                void setTooltipParams   ( const QString& _rstrTooltipParams );
+                void setRequiredLevel   ( const QString& _rstrRequiredLevel );
+                void setItemLevel       ( const QString& _rstrItemLevel );
+                void setBonusAffixes    ( const QString& _rstrBonusAffixes );
+                void setTypeName        ( const QString& _rstrTypeName );
+                void setType            ( const QString& _rstrType );
+                void setDps             ( QVariant _dps );
+                void setAttackPerSec    ( QVariant _attackPerSec );
+                void setMinDamage       ( QVariant _minDamage );
+                void setMaxDamage       ( QVariant _maxDamage );
+                void setAttributs       ( const QStringList& _rlstrAttributs );
+                void setAttributsRaw    ( QVariant _attributsRaw );
+                void setSocketEffects   ( QVariant _socketEffects );
+                void setSalvage         ( QVariant _salvage );
+                void setGems            ( QVariant _gems );
 
                 // getFunction
-                const QString& getID() const;
-                const QString& getName() const;
-                const QString& getIcon() const;
-                const QString& getDisplayColor() const;
-                const QString& getTooltipParams() const;
+                const QString&                      getID() const;
+                const QString&                      getName() const;
+                const QString&                      getIcon() const;
+                const QString&                      getDisplayColor() const;
+                const QString&                      getTooltipParams() const;
+                const QString&                      getRequiredLevel() const;
+                const QString&                      getItemLevel() const;
+                const QString&                      getBonusAffixes() const;
+                const QString&                      getTypeName() const;
+                const QString&                      getType() const;
+                const D3_Item_SubInfo&              getDps() const;
+                const D3_Item_SubInfo&              getAttackPerSec() const;
+                const D3_Item_SubInfo&              getMinDamage() const;
+                const D3_Item_SubInfo&              getMaxDamage() const;
+                const QStringList&                  getAttributs() const;
+                const D3_Item_SubInfo&              getAttributsRaw() const;
+                const D3_Item_SubInfo&              getSocketEffects() const;
+                const QList< D3_Item_Salvage* >&    getSalvage() const;
+                const QList< D3_Item_Gem* >&        getGems() const;
+
+                QVariant            getDpsAsQVariant() const;
+                QVariant            getAttackPerSecAsQVariant() const;
+                QVariant            getMinDamageAsQVariant() const;
+                QVariant            getMaxDamageAsQVariant() const;
+                QVariant            getAttributsRawAsQVariant() const;
+                QVariant            getSocketEffectsAsQVariant() const;
+                QVariant            getSalvageAsQVariant() const;
+                QVariant            getGemsAsQVariant() const;
 
                 // tools
                 virtual bool isValid() const;
@@ -43,11 +116,25 @@ namespace d3cplus
                 bool operator==( const D3_Item& _rD3_Item );
 
             private:
-                QString m_strID;
-                QString m_strName;
-                QString m_strIcon;
-                QString m_strDisplayColor;
-                QString m_strTooltipParams;
+                QString                     m_strID;
+                QString                     m_strName;
+                QString                     m_strIcon;
+                QString                     m_strDisplayColor;
+                QString                     m_strTooltipParams;
+                QString                     m_strRequiredLevel;
+                QString                     m_strItemLevel;
+                QString                     m_strBonusAffixes;
+                QString                     m_strTypeName;
+                QString                     m_strType;
+                D3_Item_SubInfo             m_dps;
+                D3_Item_SubInfo             m_attackPerSec;
+                D3_Item_SubInfo             m_minDamage;
+                D3_Item_SubInfo             m_maxDamage;
+                QStringList                 m_lstrAttributs;
+                D3_Item_SubInfo             m_attributsRaw;
+                D3_Item_SubInfo             m_SocketEffects;
+                QList< D3_Item_Salvage* >   m_lpSalvage;
+                QList< D3_Item_Gem* >       m_lpGems;
         };
     }
 }
--- d3cplus/src/d3cplus_data_item.cpp
+++ d3cplus/src/d3cplus_data_item.cpp
@@ -17,14 +17,99 @@ namespace d3cplus
         void D3_Item::setName( const QString& _rstrName )                   { m_strName             = _rstrName; }
         void D3_Item::setIcon( const QString& _strIcon )                    { m_strIcon             = _strIcon; }
         void D3_Item::setDisplayColor( const QString& _strDisplayColor )    { m_strDisplayColor     = _strDisplayColor; }
-        void D3_Item::setTooltipParams( const QString& _strTooltipParams )  { m_strTooltipParams    = _strTooltipParams; }
+        void D3_Item::setTooltipParams( const QString& _strTooltipParams )  { m_strTooltipParams    = _strTooltipParams; }                void setRequiredLevel   ( const QString& _rstrRequiredLevel );
+        void D3_Item::setRequiredLevel( const QString& _rstrRequiredLevel ) { m_strTooltipParams    = _strTooltipParams; }
+        void D3_Item::setItemLevel( const QString& _rstrItemLevel )         { m_strTooltipParams    = _strTooltipParams; }
+        void D3_Item::setBonusAffixes( const QString& _rstrBonusAffixes )   { m_strTooltipParams    = _strTooltipParams; }
+        void D3_Item::setTypeName( const QString& _rstrTypeName )           { m_strTooltipParams    = _strTooltipParams; }
+        void D3_Item::setType( const QString& _rstrType )                   { m_strTooltipParams    = _strTooltipParams; }
+        void D3_Item::setDps( QVariant _dps )
+        {
+            m_strTooltipParams    = _strTooltipParams;
+        }
+        void D3_Item::setAttackPerSec( QVariant _attackPerSec )
+        {
+            m_strTooltipParams    = _strTooltipParams;
+        }
+        void D3_Item::setMinDamage( QVariant _minDamage )
+        {
+            m_strTooltipParams    = _strTooltipParams;
+        }
+        void D3_Item::setMaxDamage( QVariant _maxDamage )
+        {
+            m_strTooltipParams    = _strTooltipParams;
+        }
+        void D3_Item::setAttributs( const QStringList& _rlstrAttributs )    { m_strTooltipParams    = _strTooltipParams; }
+        void D3_Item::setAttributsRaw( QVariant _attributsRaw )
+        {
+            m_strTooltipParams    = _strTooltipParams;
+        }
+        void D3_Item::setSocketEffects( QVariant _socketEffects )
+        {
+            m_strTooltipParams    = _strTooltipParams;
+        }
+        void D3_Item::setSalvage( QVariant _salvage )
+        {
+            m_strTooltipParams    = _strTooltipParams;
+        }
+        void D3_Item::setGems( QVariant _gems )
+        {
+            m_strTooltipParams    = _strTooltipParams;
+        }
 
         // getFunction
-        const QString& D3_Item::getID() const           { return m_strID; }
-        const QString& D3_Item::getName() const         { return m_strName; }
-        const QString& D3_Item::getIcon() const         { return m_strIcon; }
-        const QString& D3_Item::getDisplayColor() const { return m_strDisplayColor; }
-        const QString& D3_Item::getTooltipParams() const{ return m_strTooltipParams; }
+        const QString& D3_Item::getID() const                   { return m_strID; }
+        const QString& D3_Item::getName() const                 { return m_strName; }
+        const QString& D3_Item::getIcon() const                 { return m_strIcon; }
+        const QString& D3_Item::getDisplayColor() const         { return m_strDisplayColor; }
+        const QString& D3_Item::getTooltipParams() const        { return m_strTooltipParams; }
+        const QString& D3_Item::getRequiredLevel() const        { return m_strRequiredLevel; }
+        const QString& D3_Item::getItemLevel() const            { return m_strItemLevel; }
+        const QString& D3_Item::getBonusAffixes() const         { return m_strBonusAffixes; }
+        const QString& D3_Item::getTypeName() const             { return m_strTypeName; }
+        const QString& D3_Item::getType() const                 { return m_strType; }
+        const D3_Item_SubInfo& D3_Item::getDps() const          { return m_dps; }
+        const D3_Item_SubInfo& D3_Item::getAttackPerSec() const { return m_attackPerSec; }
+        const D3_Item_SubInfo& D3_Item::getMinDamage() const    { return m_minDamage; }
+        const D3_Item_SubInfo& D3_Item::getMaxDamage() const    { return m_maxDamage; }
+        const QStringList& D3_Item::getAttributs() const        { return m_lstrAttributs; }
+        const D3_Item_SubInfo& D3_Item::getAttributsRaw() const { return m_attributsRaw; }
+        const D3_Item_SubInfo& D3_Item::getSocketEffects() const{ return m_socketEffects; }
+        QList< D3_Item_Salvage* > D3_Item::getSalvage() const   { return m_lpSalvage; }
+        const QList< D3_Item_Gem* >& D3_Item::getGems() const   { return m_lpGems; }
+
+        QVariant D3_Item::getDpsAsQVariant() const
+        {
+            return QVariant();
+        }
+        QVariant D3_Item::getAttackPerSecAsQVariant() const
+        {
+            return QVariant();
+        }
+        QVariant D3_Item::getMinDamageAsQVariant() const
+        {
+            return QVariant();
+        }
+        QVariant D3_Item::getMaxDamageAsQVariant() const
+        {
+            return QVariant();
+        }
+        QVariant D3_Item::getAttributsRawAsQVariant() const
+        {
+            return QVariant();
+        }
+        QVariant D3_Item::getSocketEffectsAsQVariant() const
+        {
+            return QVariant();
+        }
+        QVariant D3_Item::getSalvageAsQVariant() const
+        {
+            return QVariant();
+        }
+        QVariant D3_Item::getGemsAsQVariant() const
+        {
+            return QVariant();
+        }
 
         // tools
         bool D3_Item::isValid() const { return true; }
